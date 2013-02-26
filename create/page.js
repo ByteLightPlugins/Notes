929f2b2e02afb53f52cb031add8ed7b65c32f91f
ByteLight Plugin Repositories <plugins@bytelight.com>
2013-02-26 11:41:21 -0800

    Released on: 2013-02-26 19:41:10 +0000

--- create/page.js
+++ create/page.js
@@ -1,6 +1,10 @@
 $(function () {
   $('#save-button').click(function (e) {
-    var content = {body:$('#note-body').val(), author:$('#note-author').val()};
+    var content = {
+      body:$('#note-body').val(), 
+      author:$('#note-author').val()
+      
+    };
     BL.createContent(JSON.stringify(content));
   })
 });
